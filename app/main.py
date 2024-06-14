3226d1866dd99312db494f249af5065002822dce
Klukin_Pavel <klukin0202@yandex.ru>
2024-06-14 19:38:57 +0300

    Added server dir, database dir, gitigore file

---
+++ app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Пример пользовательских данных (для демонстрационных целей)
+fake_users = {
+    1: {"username": "john_doe", "email": "john@example.com"},
+    2: {"username": "jane_smith", "email": "jane@example.com"},
+}
+
+
+# Конечная точка для получения информации о пользователе по ID
+@app.get("/users/{user_id}")
+def read_user(user_id: int):
+    if user_id in fake_users:
+        return fake_users[user_id]
+    return {"error": "User not found"}
+
+
+@app.get("/users/")
+def read_users(limit: int = 10):
+    return dict(list(fake_users.items())[:limit])
