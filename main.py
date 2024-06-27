b1e4941445a3f9bb8ee43a3746c8f0bb69859a86
Klukin_Pavel <klukin0202@yandex.ru>
2024-06-27 10:09:20 +0300

    Добавил страницы регистрации и входа. Надо обработать две кноки регистрации и входа на первой странице сайта, в зависимости от их нажатия выкидывать на нужную страницу.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -9,7 +8,28 @@ domain_template_directory = "templates"
 templates = Jinja2Templates(directory=domain_template_directory)
 
 
+@app.get("/first_page/", response_class=HTMLResponse)
+async def create_user(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse("first_page.html", context)
+# надо обработать две кнопки "регистрация" и "вход" и потом выкидывать на нужную страницу
+# после регистрации выкидывать на первую страницу, сообщив об успешной регистрации
+
+
+@app.get("/sign_up/", response_class=HTMLResponse)
+async def create_user(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse("sign_up.html", context)
+
+
 @app.get("/sign_in/", response_class=HTMLResponse)
 async def sign_in(request: Request):
     context = {"request": request}
     return templates.TemplateResponse("sign_in.html", context)
+
+
+@app.get("/account/", response_class=HTMLResponse)
+async def create_user(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse("account.html", context)
+
