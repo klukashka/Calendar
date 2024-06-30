c681745baa082bbe6a6ec3badaeeb5c8b8bdee2b
Klukin_Pavel <klukin0202@yandex.ru>
2024-06-30 10:44:43 +0300

    Добавил первую страницу сайта для перехода в sign_in и sign_up. Для sign_in сделал ввод данных с последующим переходом в личный кабинет. Пока что всё работает только для sign_in и для одного пользователя.

--- main.py
+++ main.py
@@ -1,35 +1,37 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Form, Response
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-domain_template_directory = "templates"
-templates = Jinja2Templates(directory=domain_template_directory)
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/first_page/", response_class=HTMLResponse)
-async def create_user(request: Request):
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
     context = {"request": request}
-    return templates.TemplateResponse("first_page.html", context)
-# надо обработать две кнопки "регистрация" и "вход" и потом выкидывать на нужную страницу
-# после регистрации выкидывать на первую страницу, сообщив об успешной регистрации
+    return templates.TemplateResponse("index.html", context)
+# пожалуй, стоит сделать функцию redirect_to_page внутри бэкенда
 
 
-@app.get("/sign_up/", response_class=HTMLResponse)
-async def create_user(request: Request):
-    context = {"request": request}
-    return templates.TemplateResponse("sign_up.html", context)
-
-
-@app.get("/sign_in/", response_class=HTMLResponse)
-async def sign_in(request: Request):
+@app.get("/sign_in", response_class=HTMLResponse)
+async def sign_in_page(request: Request):
     context = {"request": request}
     return templates.TemplateResponse("sign_in.html", context)
 
 
-@app.get("/account/", response_class=HTMLResponse)
-async def create_user(request: Request):
+@app.post("/sign_in")
+async def sign_in_user(response: Response, username: str = Form(...), password: str = Form(...)):
+    if username == "admin" and password == "123":
+        return RedirectResponse(url="/account", status_code=303)
+    else:
+        return "Invalid Data"
+
+
+@app.get("/account", response_class=HTMLResponse)
+async def account_inside(request: Request):
+    # session = request.cookies.get("session")
+    # if session != "authenticated":
+    #     return "Not authenticated"
     context = {"request": request}
     return templates.TemplateResponse("account.html", context)
-
