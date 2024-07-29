a5221d067161273218ac13feef2466fc48ff75a8
klukashka <klukin0202@yandex.ru>
2024-07-30 02:53:38 +0300

    Added Note table

--- models/models.py
+++ models/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON, Boolean
+from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, Boolean, DateTime
 
 metadata = MetaData()
 
@@ -15,4 +15,15 @@ user = Table(
     Column("is_active", Boolean, default=True, nullable=False),
     Column("is_superuser", Boolean, default=False, nullable=False),
     Column("is_verified", Boolean, default=False, nullable=False),
+)
+
+note = Table(
+    "note",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("user_id", Integer),
+    Column("remind_time", String, nullable=False),
+    Column("message", String, nullable=True),
+    Column("important", Boolean, nullable=True),
+    Column("is_completed", Boolean, nullable=False)
 )
