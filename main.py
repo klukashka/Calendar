1670fd4d1fbeb4622de12f3d3f21b7ccd46d5212
klukashka <klukin0202@yandex.ru>
2024-08-02 02:24:22 +0300

    Made create_note function work properly (so that it saves the data to the database)

--- main.py
+++ main.py
@@ -2,22 +2,20 @@ from typing import Annotated
 
 from datetime import datetime
 
-from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 from fastapi import FastAPI, Depends, Request, Form, Body, HTTPException
 from fastapi_users import FastAPIUsers
 
 from auth.auth import auth_backend
-from db.database import User
+from db.database import User, Note, get_async_session
 from auth.manager import get_user_manager
-from auth.schemas import UserRead, UserCreate
+from schemas.schemas import *
 
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from models.models import note
 
-from db.database import get_user_db
 
 templates = Jinja2Templates(directory="templates")
 
@@ -41,29 +39,44 @@ app.include_router(
     tags=["auth"],
 )
 
-# app.include_router(notes_router)
-
 current_active_user = fastapi_users.current_user(active=True)
 
 
 @app.get("/account")
-def account(user: User = Depends(current_active_user)):
+async def account(user: User = Depends(current_active_user)):
     return f"Hello, {user.username}"
 
 
-@app.get("/account/create")
-async def note_create(remind_time: Annotated[str, Body(embed=True)],
-                      message: Annotated[str, Form()],
-                      important: Annotated[bool, Form()],
-                      db_session: Session = Depends(get_user_db)
-                      ):
+# @app.post("/account/create", response_model=NoteRead)
+# async def note_create(db_session: Session, note_to_create: NoteCreate, user: User = Depends(current_active_user)):
+#     date_time_format = "%H:%M %d.%m.%Y"
+#     if datetime.strptime(note_create.remind_time, date_time_format):
+#         raise HTTPException(status_code=406, detail="Wrong date-time data format")
+#     new_note = Note(user_id=user.id,
+#                     remind_time=note_to_create.remind_time,
+#                     message=note_to_create.message,
+#                     important=note_to_create.important,
+#                     is_complited=False)
+#     db_session.add(new_note)
+#     db_session.commit()
+#     db_session.refresh(new_note)
+#     return new_note
+
+
+@app.post("/account/note_create")
+async def note_create(note_to_create: NoteCreate,
+                      db_session: AsyncSession = Depends(get_async_session),
+                      user: User = Depends(current_active_user)
+                      ) -> None:
     date_time_format = "%H:%M %d.%m.%Y"
-    if datetime.strptime(remind_time, date_time_format):
+    if not datetime.strptime(note_to_create.remind_time, date_time_format):
         raise HTTPException(status_code=406, detail="Wrong date-time data format")
-    new_note = note(user_id=current_active_user.id,
-                    remind_time=remind_time,
-                    message=message,
-                    important=important,
-                    is_complited=False)
+    new_note = Note(user_id=user.id,
+                    remind_time=note_to_create.remind_time,
+                    message=note_to_create.message,
+                    important=note_to_create.important,
+                    is_completed=False)
     db_session.add(new_note)
-    db_session.commit()
+    await db_session.commit()
+
+#20:30 20.12.2024
