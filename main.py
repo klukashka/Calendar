a4d6e332a76150065cb53970718d4cf6b8f77050
klukashka <klukin0202@yandex.ru>
2024-08-13 12:44:06 +0300

    NoteCreate updated and NoteGet added

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ from auth.manager import get_user_manager
 from schemas.schemas import *
 
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 from models.models import note
-
 
 templates = Jinja2Templates(directory="templates")
 
@@ -42,16 +42,16 @@ app.include_router(
 current_active_user = fastapi_users.current_user(active=True)
 
 
-@app.get("/account")
-async def account(user: User = Depends(current_active_user)):
-    return f"Hello, {user.username}"
+# @app.get("/account")
+# async def account(user: User = Depends(current_active_user)):
+#     return f"Hello, {user.username}"
 
 
-@app.post("/account/note_create")
+@app.post("/account", status_code=201, response_model=NoteCreate)
 async def note_create(note_to_create: NoteCreate,
                       db_session: AsyncSession = Depends(get_async_session),
                       user: User = Depends(current_active_user)
-                      ) -> None:
+                      ) -> NoteCreate:
     date_time_format = "%H:%M %d.%m.%Y"
     if not datetime.strptime(note_to_create.remind_time, date_time_format):
         raise HTTPException(status_code=406, detail="Wrong date-time data format")
@@ -62,5 +62,19 @@ async def note_create(note_to_create: NoteCreate,
                     is_completed=False)
     db_session.add(new_note)
     await db_session.commit()
+    return note_to_create
 
-#20:30 20.12.2024
+
+# 20:30 20.12.2024
+# 20:40 20.12.2024
+
+
+# here user chooses a note and updates it
+
+@app.get("/account")
+async def get_notes(db_session: AsyncSession = Depends(get_async_session),
+                    user: User = Depends(current_active_user)
+                    ):
+    rows = await db_session.execute(select(Note).where(Note.user_id == user.id))
+    rows = list(rows.scalars())
+    return rows
