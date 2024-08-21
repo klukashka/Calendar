10ca9dd43f1c5ac8acf9f6fbd02b7b1228d28a1f
klukashka <klukin0202@yandex.ru>
2024-08-21 18:12:42 +0300

    Several temporary changes

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from typing import Annotated
-
 from datetime import datetime
 
-from fastapi.templating import Jinja2Templates
+from utils.EmailSender import EmailSender
 
 from fastapi import FastAPI, Depends, Request, Form, Body, HTTPException
 from fastapi_users import FastAPIUsers
@@ -10,16 +8,18 @@ from fastapi_users import FastAPIUsers
 from auth.auth import auth_backend
 from db.database import User, Note, get_async_session
 from auth.manager import get_user_manager
-from schemas.schemas import *
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 
-from models.models import note
+from schemas.schemas import *
 
-templates = Jinja2Templates(directory="templates")
 
 app = FastAPI(title="Calendar")
+
+email_sender = EmailSender()
+email_sender.start()
+# email_sender.send("klukin0202@yandex.ru", "mailcheck", "it's me")
 
 fastapi_users = FastAPIUsers[User, int](
     get_user_manager,
@@ -42,11 +42,6 @@ app.include_router(
 current_active_user = fastapi_users.current_user(active=True)
 
 
-# @app.get("/account")
-# async def account(user: User = Depends(current_active_user)):
-#     return f"Hello, {user.username}"
-
-
 @app.post("/account", status_code=201, response_model=NoteCreate)
 async def note_create(note_to_create: NoteCreate,
                       db_session: AsyncSession = Depends(get_async_session),
@@ -55,8 +50,11 @@ async def note_create(note_to_create: NoteCreate,
     date_time_format = "%H:%M %d.%m.%Y"
     if not datetime.strptime(note_to_create.remind_time, date_time_format):
         raise HTTPException(status_code=406, detail="Wrong date-time data format")
+    converted_remind_time = datetime.strptime(note_to_create.remind_time, "%H:%M %d.%m.%Y")
+    if converted_remind_time < datetime.now():
+        raise HTTPException(status_code=406, detail="Inappropriate time value")
     new_note = Note(user_id=user.id,
-                    remind_time=note_to_create.remind_time,
+                    remind_time=converted_remind_time,
                     message=note_to_create.message,
                     important=note_to_create.important,
                     is_completed=False)
@@ -68,13 +66,11 @@ async def note_create(note_to_create: NoteCreate,
 # 20:30 20.12.2024
 # 20:40 20.12.2024
 
-
-# here user chooses a note and updates it
-
 @app.get("/account")
 async def get_notes(db_session: AsyncSession = Depends(get_async_session),
                     user: User = Depends(current_active_user)
                     ):
     rows = await db_session.execute(select(Note).where(Note.user_id == user.id))
     rows = list(rows.scalars())
+    print(type(rows))
     return rows
