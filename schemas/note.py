3bd1606189a3bc8f0a9103cb0edb955a26406d93
klukashka <klukin0202@yandex.ru>
2024-08-26 02:10:00 +0300

    Separated schemas for users to auth and reg from schemas for notes

--- schemas/note.py
+++ schemas/note.py
@@ -4,37 +4,12 @@ from fastapi_users import schemas
 from pydantic import BaseModel
 
 
-class UserBase(BaseModel):
-    email: int
-    username: int
-    class Config:
-        from_attributes = True
-
-
 class NoteBase(BaseModel):
     remind_time: str
     message: str
     important: bool
     class Config:
         from_attributes = True
-
-
-class UserCreate(UserBase, schemas.BaseUserCreate):
-    email: str
-    username: str
-    password: str
-    is_active: Optional[bool] = True # what's the use of None (Optional) for this field?
-    is_superuser: Optional[bool] = False
-    is_verified: Optional[bool] = False
-
-
-class UserRead(UserBase, schemas.BaseUser[int]):
-    id: int
-    email: str
-    username: str
-    is_active: bool = True
-    is_superuser: bool = False
-    is_verified: bool = False
 
 
 class NoteCreate(NoteBase):
