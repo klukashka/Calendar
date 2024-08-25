19e8836b6371f740e154f7bc2d2abea879bfc222
klukashka <klukin0202@yandex.ru>
2024-08-26 02:07:37 +0300

    Made a raw async main function and made an entry point

--- main.py
+++ main.py
@@ -1,76 +1,87 @@
-from datetime import datetime
-
-from utils.EmailSender import EmailSender
-
+import asyncio
+import uvicorn
+import core.routers.account_router
+from db.connector import setup_get_pool
 from fastapi import FastAPI, Depends, Request, Form, Body, HTTPException
 from fastapi_users import FastAPIUsers
-
 from auth.auth import auth_backend
-from db.database import User, Note, get_async_session
-from auth.manager import get_user_manager
+from models.models import User
+from auth.manager import get_user_manager, get_async_session
+from schemas.auth import *
+from config import DATABASE_URL
 
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+async def main() -> None:
+    """Main function to run app"""
+    app = FastAPI(title="Calendar")
 
-from schemas.schemas import *
+    fastapi_users = FastAPIUsers[User, int](
+        get_user_manager,
+        [auth_backend],
+    )
+
+    app.include_router(
+        fastapi_users.get_auth_router(auth_backend),
+        prefix="/auth/jwt",
+        tags=["auth"],
+    )
+
+    app.include_router(
+        fastapi_users.get_register_router(UserRead, UserCreate),
+        prefix="/auth",
+        tags=["auth"],
+    )
+
+    app.include_router(
+        core.routers.account_router.get_account_router(),
+        prefix="/account",
+        tags=["account"]
+    )
+
+    # connect db
+    session_pool = await setup_get_pool(DATABASE_URL)
+
+    #
+
+    config = uvicorn.Config("main:app", port=5000, log_level="info")
+    server = uvicorn.Server(config)
+    await server.serve()
 
 
-app = FastAPI(title="Calendar")
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        pass
 
-email_sender = EmailSender()
-email_sender.start()
-# email_sender.send("klukin0202@yandex.ru", "mailcheck", "it's me")
-
-fastapi_users = FastAPIUsers[User, int](
-    get_user_manager,
-    [auth_backend],
-)
-
-# https://github.com/fastapi-users/fastapi-users/blob/master/fastapi_users/router/auth.py
-app.include_router(
-    fastapi_users.get_auth_router(auth_backend),
-    prefix="/auth/jwt",
-    tags=["auth"],
-)
 # https://github.com/fastapi-users/fastapi-users/blob/master/fastapi_users/router/register.py
-app.include_router(
-    fastapi_users.get_register_router(UserRead, UserCreate),
-    prefix="/auth",
-    tags=["auth"],
-)
-
-current_active_user = fastapi_users.current_user(active=True)
+# https://github.com/fastapi-users/fastapi-users/blob/master/fastapi_users/router/auth.py
 
 
-@app.post("/account", status_code=201, response_model=NoteCreate)
-async def note_create(note_to_create: NoteCreate,
-                      db_session: AsyncSession = Depends(get_async_session),
-                      user: User = Depends(current_active_user)
-                      ) -> NoteCreate:
-    date_time_format = "%H:%M %d.%m.%Y"
-    if not datetime.strptime(note_to_create.remind_time, date_time_format):
-        raise HTTPException(status_code=406, detail="Wrong date-time data format")
-    converted_remind_time = datetime.strptime(note_to_create.remind_time, "%H:%M %d.%m.%Y")
-    if converted_remind_time < datetime.now():
-        raise HTTPException(status_code=406, detail="Inappropriate time value")
-    new_note = Note(user_id=user.id,
-                    remind_time=converted_remind_time,
-                    message=note_to_create.message,
-                    important=note_to_create.important,
-                    is_completed=False)
-    db_session.add(new_note)
-    await db_session.commit()
-    return note_to_create
-
-
-# 20:30 20.12.2024
-# 20:40 20.12.2024
-
-@app.get("/account")
-async def get_notes(db_session: AsyncSession = Depends(get_async_session),
-                    user: User = Depends(current_active_user)
-                    ):
-    rows = await db_session.execute(select(Note).where(Note.user_id == user.id))
-    rows = list(rows.scalars())
-    print(type(rows))
-    return rows
+# make one more router for account handler to keep there those ^ functions
+# understand how to throw db_session to a function without throwing it
+# make a good main func
+# understand how to deal with a database and orm
+# do smth with a scheduler
+# git: how to move file to another folder without losing its history of changing and commits?
+# add logging
+# how to get rid of those table-classes
+# what is metadata?
+# what is __call__ method? makes an object behave like a func
+# why there was db_urI????????? not urL
+# what is "future" parameter in create_async_engine
+# what is engine.begin()
+# what is context manager
+# why to use __repr__ in database classes
+# how to select files for .gitignore
+# about config in .env and python logging
+# what to do with get_async_session
+# what are logs written to?
+# what is MIT license
+# if I rearrange files, all the imports get wrong. What should I do?
+# what is model_db_mixin
+# learn how to Makefile
+# how make correct .env file
+# where to store secret_key generator for password hashing (or how to generate it)
+# __main__
+# where is fastapi.db SQLAlchemyUserDatabase in docs?
+# Don't forget to ask about server
