4be0d2b87f3b2dab7a37c85fac4dc3ad83ad38b0
klukashka <klukin0202@yandex.ru>
2024-08-26 02:36:31 +0300

    Modified

--- auth/manager.py
+++ auth/manager.py
@@ -1,16 +1,26 @@
-from typing import Optional
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import Depends
+from fastapi_users import BaseUserManager, IntegerIDMixin
+from models.models import User
+from fastapi_users.db import SQLAlchemyUserDatabase
+from config import SECRET_KEY
 
-from fastapi import Depends, Request
-from fastapi import Depends, Request, Response
-from fastapi_users import BaseUserManager, IntegerIDMixin, exceptions, models, schemas
-
-from db.database import User, get_user_db
-SECRET = "SECRET"
 
 
 class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
-    reset_password_token_secret = SECRET
-    verification_token_secret = SECRET
+    """User info manager (concerning registration and authentication)"""
+    reset_password_token_secret = SECRET_KEY
+    verification_token_secret = SECRET_KEY
+
+
+async def get_async_session(async_session_maker) -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
 
 
 async def get_user_manager(user_db=Depends(get_user_db)):
