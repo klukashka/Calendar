8b474e45e92d884fb7ba27eef72f55de5ac7c8ae
klukashka <klukin0202@yandex.ru>
2024-08-31 15:43:58 +0300

    Added entry point, session pool and took an account router into another folder

--- main.py
+++ main.py
@@ -1,87 +1,58 @@
 import asyncio
 import uvicorn
-import core.routers.account_router
 from db.connector import setup_get_pool
-from fastapi import FastAPI, Depends, Request, Form, Body, HTTPException
+from fastapi import FastAPI
 from fastapi_users import FastAPIUsers
 from auth.auth import auth_backend
 from models.models import User
-from auth.manager import get_user_manager, get_async_session
-from schemas.auth import *
-from config import DATABASE_URL
+from config import TITLE, DB_URL
+from schemas.user import UserRead, UserCreate
+from core.routers import account_router
+from db.repositories.note import NoteRepo
+from db.repositories.user import UserRepo
+
+from auth.manager import providing_user_manager
+
 
 async def main() -> None:
     """Main function to run app"""
-    app = FastAPI(title="Calendar")
+    app = FastAPI(title=TITLE)
 
-    fastapi_users = FastAPIUsers[User, int](
-        get_user_manager,
+    session_pool = await setup_get_pool(DB_URL)
+
+    async with session_pool() as _session:
+        user_repo = UserRepo(session=_session)
+        note_repo = NoteRepo(session=_session)
+
+    _users = FastAPIUsers[User, int](
+        await providing_user_manager(session_pool),
         [auth_backend],
     )
 
     app.include_router(
-        fastapi_users.get_auth_router(auth_backend),
+        _users.get_auth_router(auth_backend),
         prefix="/auth/jwt",
         tags=["auth"],
     )
 
     app.include_router(
-        fastapi_users.get_register_router(UserRead, UserCreate),
+        _users.get_register_router(UserRead, UserCreate),
         prefix="/auth",
         tags=["auth"],
     )
 
     app.include_router(
-        core.routers.account_router.get_account_router(),
+        account_router.get_account_router(_users, user_repo, note_repo),
         prefix="/account",
         tags=["account"]
     )
 
-    # connect db
-    session_pool = await setup_get_pool(DATABASE_URL)
-
-    #
-
-    config = uvicorn.Config("main:app", port=5000, log_level="info")
+    config = uvicorn.Config(app, log_level="info", reload=True)
     server = uvicorn.Server(config)
     await server.serve()
-
 
 if __name__ == "__main__":
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
         pass
-
-# https://github.com/fastapi-users/fastapi-users/blob/master/fastapi_users/router/register.py
-# https://github.com/fastapi-users/fastapi-users/blob/master/fastapi_users/router/auth.py
-
-
-# make one more router for account handler to keep there those ^ functions
-# understand how to throw db_session to a function without throwing it
-# make a good main func
-# understand how to deal with a database and orm
-# do smth with a scheduler
-# git: how to move file to another folder without losing its history of changing and commits?
-# add logging
-# how to get rid of those table-classes
-# what is metadata?
-# what is __call__ method? makes an object behave like a func
-# why there was db_urI????????? not urL
-# what is "future" parameter in create_async_engine
-# what is engine.begin()
-# what is context manager
-# why to use __repr__ in database classes
-# how to select files for .gitignore
-# about config in .env and python logging
-# what to do with get_async_session
-# what are logs written to?
-# what is MIT license
-# if I rearrange files, all the imports get wrong. What should I do?
-# what is model_db_mixin
-# learn how to Makefile
-# how make correct .env file
-# where to store secret_key generator for password hashing (or how to generate it)
-# __main__
-# where is fastapi.db SQLAlchemyUserDatabase in docs?
-# Don't forget to ask about server
