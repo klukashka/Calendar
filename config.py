cdfa9674b4bdb3a11f3836c2e722e80a05fe0140
klukashka <klukin0202@yandex.ru>
2024-08-31 15:54:45 +0300

    New constants added: TITLE, DB_URL, DATE_TIME_FORMAT, SECRET_KEY

---
+++ config.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DATE_TIME_FORMAT = os.environ.get("DATE_TIME_FORMAT")
+DB_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+TITLE = os.environ.get("TITLE")
