df65843b0467c9edae8d754613cf997b1ac849d4
klukashka <klukin0202@yandex.ru>
2024-08-31 16:39:42 +0300

    Auth backend

---
+++ auth/auth.py
@@ -0,0 +1,16 @@
+from fastapi_users.authentication import CookieTransport, AuthenticationBackend
+from fastapi_users.authentication import JWTStrategy
+from config import SECRET_KEY
+
+cookie_transport = CookieTransport(cookie_name="TODOes", cookie_max_age=3600)
+
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=SECRET_KEY, lifetime_seconds=3600)
+
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=cookie_transport,
+    get_strategy=get_jwt_strategy,
+)
