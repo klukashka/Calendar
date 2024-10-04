2cd8f987a7d94519ad8dc1eef928baa3c2718537
klukashka <klukin0202@yandex.ru>
2024-10-05 01:10:56 +0300

    Reformatted the scopes in the header of note_create

--- app/core/routers/account_router.py
+++ app/core/routers/account_router.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends
 from fastapi_users import FastAPIUsers
 from app.schemas.note import NoteCreate
 from app.schemas.user import UserRead
-from app.db.repositories.note import NoteRepo, convert_db_note_to_read_note
+from app.db.repositories.note import NoteRepo, _convert_db_note_to_read_note
 from app.db.repositories.user import UserRepo
 
 
@@ -16,11 +16,12 @@ async def get_account_router(users: FastAPIUsers, async_session_maker: async_ses
         user_repo = UserRepo(_session)
 
         @router.post("/account/note_create", status_code=201)
-        async def note_create(note_to_create: NoteCreate,
-                              user: UserRead = Depends(users.current_user(active=True))
-                              ):
+        async def note_create(
+                note_to_create: NoteCreate,
+                user: UserRead = Depends(users.current_user(active=True))
+              ):
             note = await note_repo.add_note(note_to_create, user.id)
-            note_to_read = convert_db_note_to_read_note(note)
+            note_to_read = _convert_db_note_to_read_note(note)
             return note_to_read
 
         @router.get("/account/notes_get")
