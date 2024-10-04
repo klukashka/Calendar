bb5966de799a9fdb9451f7a1113db8d9f1c1f22c
klukashka <klukin0202@yandex.ru>
2024-10-05 01:42:01 +0300

    Async function to include all the routers

---
+++ app/core/routers_includer.py
@@ -0,0 +1,26 @@
+from app.schemas.user import UserRead, UserCreate
+from app.core.routers import account_router
+from app.auth.auth import auth_backend
+from fastapi import FastAPI
+from fastapi_users import FastAPIUsers
+from app.models.User import User
+from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
+
+
+async def include_routers(app: FastAPI, users: FastAPIUsers[User, int], session_pool: async_sessionmaker[AsyncSession]):
+    app.include_router(
+        users.get_auth_router(auth_backend),
+        prefix="/auth/jwt",
+        tags=["auth"],
+    )
+
+    app.include_router(
+        users.get_register_router(UserRead, UserCreate),
+        prefix="/auth",
+        tags=["auth"],
+    )
+
+    app.include_router(
+        await account_router.get_account_router(users, session_pool),
+        tags=["account"]
+    )
