61347bb1ca40a82feb092bfc9edd1cd43e0823fc
klukashka <klukin0202@yandex.ru>
2024-10-07 22:26:25 +0300

    Got rid of db variables. Only db_url

--- app/config.py
+++ app/config.py
@@ -3,18 +3,13 @@ import os
 
 load_dotenv()
 
-DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT")
-DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
-DB_PASS = os.environ.get("DB_PASS")
+DB_URL = os.environ.get("DB_URL")
 BACK_HOST = os.environ.get("BACK_HOST")
 BACK_PORT = os.environ.get("BACK_PORT")
 FRONT_HOST = os.environ.get("FRONT_HOST")
 FRONT_PORT = os.environ.get("FRONT_PORT")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DATE_TIME_FORMAT = os.environ.get("DATE_TIME_FORMAT")
-DB_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 SMTP_SERVER=os.environ.get("SMTP_SERVER")
 SMTP_PORT=os.environ.get("SMTP_PORT")
