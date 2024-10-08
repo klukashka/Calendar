63619eabad2c4a698e8a3edd14f211c1263f7ac9
klukashka <klukin0202@yandex.ru>
2024-10-08 14:15:26 +0300

    Removed excess variables. Only db_url

--- migrations/env.py
+++ migrations/env.py
@@ -3,20 +3,13 @@ from logging.config import fileConfig
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
-from app.config import DB_HOST, DB_PORT, DB_PASS, DB_USER, DB_NAME
+from app.config import DB_URL
 from alembic import context
 from app.db.base import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
-section = config.config_ini_section
-config.set_section_option(section, "DB_HOST", DB_HOST)
-config.set_section_option(section, "DB_PORT", DB_PORT)
-config.set_section_option(section, "DB_USER", DB_USER)
-config.set_section_option(section, "DB_NAME", DB_NAME)
-config.set_section_option(section, "DB_PASS", DB_PASS)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -25,7 +18,7 @@ if config.config_file_name is not None:
 
 target_metadata = Base.metadata
 
-db_url = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+db_url = DB_URL
 config.set_main_option("sqlalchemy.url", db_url)
 
 # other values from the config, defined by the needs of env.py,
