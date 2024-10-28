c244aa7ca262c670ea479bd20b237eae7377be77
klukashka <klukin0202@yandex.ru>
2024-10-29 01:32:09 +0300

    Time zone functionality was turned off temporarily

--- app/db/repositories/email.py
+++ app/db/repositories/email.py
@@ -11,43 +11,54 @@ from app.schemas.email import EmailRead
 
 class EmailRepo:
     """Repository for database operations concerning emails"""
+
     def __init__(self, session: AsyncSession):
         self._session = session
 
     async def get_notes_to_send(self, low: int, high: int) -> AsyncGenerator[EmailRead, None]:
-        """Notes which should be sent (time to remind)"""
+        """Get notes which should be sent (time to remind)"""
         try:
-            current_time = datetime.now().replace(microsecond=0, tzinfo=timezone(offset=timedelta()))
-
+            current_time = datetime.now()
             query = (
                 select(DBNote.message, DBUser.email, DBUser.nickname)
-                .join(DBNote, (DBUser.id == DBNote.user_id)) # type: ignore
-                .where(DBNote.remind_time < current_time)
-                .limit(high).offset(low)
+                .join(DBNote, (DBUser.id == DBNote.user_id))  # type: ignore
+                .where((DBNote.remind_time <= current_time) &
+                       (DBNote.is_completed == False) &
+                       (DBNote.important == True))
+                .limit(high)
+                .offset(low)
             )
-            # ----- mark as completed ---------
-            await self.update_expired_notes()
-            # ---------------------------------
             for row in await self._session.execute(query):
-                yield _convert_db_email_to_read_email(row)
-        # other exceptions
+                yield self._convert_db_email_to_read_email(row)
+
+            # ------- mark as completed -------
+            await self.update_expired_notes(low, high)
+            # ---------------------------------
+
         except SQLAlchemyError:
             raise DBError("Failed to get notes from the database") from SQLAlchemyError
 
-    async def update_expired_notes(self):
+    async def update_expired_notes(self, low: int, high: int):
         """Mark all the expired notes as completed"""
-        current_time = datetime.now().replace(microsecond=0, tzinfo=timezone(offset=timedelta()))
-        update_query = (update(DBNote)
-            .where(
-            (DBNote.is_completed == False) and (DBNote.remind_time >= current_time))
+        current_time = datetime.now()
+        sub_query = (
+            select(DBNote.id)
+            .where((DBNote.is_completed == False) & (DBNote.remind_time <= current_time))
+            .limit(high)
+            .offset(low)
+        )
+        update_query = (
+            update(DBNote)
+            .filter(DBNote.id.in_(sub_query))
             .values(is_completed=True)
         )
         await self._session.execute(update_query)
         await self._session.commit()
 
-def _convert_db_email_to_read_email(db_email) -> EmailRead:
-    return EmailRead(
-        message=str(db_email.message) if db_email.message else None,
-        email=str(db_email.email) if db_email.email else None,
-        nickname=str(db_email.nickname) if db_email.nickname else None,
-    )
+    @staticmethod
+    def _convert_db_email_to_read_email(db_email) -> EmailRead:
+        return EmailRead(
+            message=str(db_email.message) if db_email.message else None,
+            email=str(db_email.email) if db_email.email else None,
+            nickname=str(db_email.nickname) if db_email.nickname else None,
+        )
