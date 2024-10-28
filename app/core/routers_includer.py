d7a32c0cb504888d2c38ccf9e52595c5ab790f89
klukashka <klukin0202@yandex.ru>
2024-10-29 01:32:54 +0300

    Redis was added

--- app/core/routers_includer.py
+++ app/core/routers_includer.py
@@ -1,3 +1,4 @@
+from app.db.redis_storage import RedisStorage
 from app.schemas.user import UserRead, UserCreate
 from app.core.routers import account_router
 from app.auth.auth import auth_backend
@@ -7,7 +8,12 @@ from app.models.User import User
 from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
 
 
-async def include_routers(app: FastAPI, users: FastAPIUsers[User, int], session_pool: async_sessionmaker[AsyncSession]):
+async def include_routers(
+        app: FastAPI,
+        users: FastAPIUsers[User, int],
+        session_pool: async_sessionmaker[AsyncSession],
+        redis_pool: RedisStorage,
+):
     app.include_router(
         users.get_auth_router(auth_backend),
         prefix="/auth/jwt",
@@ -21,6 +27,6 @@ async def include_routers(app: FastAPI, users: FastAPIUsers[User, int], session_
     )
 
     app.include_router(
-        await account_router.get_account_router(users, session_pool),
+        await account_router.get_account_router(users, session_pool, redis_pool),
         tags=["account"]
     )
