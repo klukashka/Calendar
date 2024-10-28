5402c2f9f19d1601b0785adf7afc239df1372ad7
klukashka <klukin0202@yandex.ru>
2024-10-29 01:03:08 +0300

    Time zone is turned off temporarily

--- app/models/User.py
+++ app/models/User.py
@@ -11,7 +11,7 @@ class User(SQLAlchemyBaseUserTable[int], Base):
     id = Column(BigInteger, primary_key=True)
     email = Column(String, nullable=False)
     nickname = Column(String, nullable=False)
-    registered_at = Column(DateTime(timezone=True), default=func.now())
+    registered_at = Column(DateTime(timezone=False), default=func.now())
     hashed_password: str = Column(String(length=1024), nullable=False)
     is_active: bool = Column(Boolean, default=True, nullable=False)
     is_superuser: bool = Column(Boolean, default=False, nullable=False)
