9caa1d59113814f8f51c908f0753f5468aab4624
klukashka <klukin0202@yandex.ru>
2024-10-29 00:54:54 +0300

    Added new fields for Redis and LOG_FILE field

--- app/config.py
+++ app/config.py
@@ -16,11 +16,18 @@ BACK_PORT = os.environ.get("BACK_PORT")
 FRONT_HOST = os.environ.get("FRONT_HOST")
 FRONT_PORT = os.environ.get("FRONT_PORT")
 SECRET_KEY = os.environ.get("SECRET_KEY")
+
+REDIS_HOST = os.environ.get('REDIS_HOST')
+REDIS_PORT = os.environ.get('REDIS_PORT')
+REDIS_DB = os.environ.get('REDIS_DB')
+REDIS_PASS = os.environ.get("REDIS_PASS")
+
 DATE_TIME_FORMAT = os.environ.get("DATE_TIME_FORMAT")
 
-SMTP_SERVER=os.environ.get("SMTP_SERVER")
-SMTP_PORT=os.environ.get("SMTP_PORT")
-ADMIN_EMAIL=os.environ.get("ADMIN_EMAIL")
-ADMIN_EMAIL_PASSWORD=os.environ.get("ADMIN_EMAIL_PASSWORD")
+SMTP_SERVER = os.environ.get("SMTP_SERVER")
+SMTP_PORT = os.environ.get("SMTP_PORT")
+ADMIN_EMAIL = os.environ.get("ADMIN_EMAIL")
+ADMIN_EMAIL_PASSWORD = os.environ.get("ADMIN_EMAIL_PASSWORD")
 
-LOG_LEVEL=os.environ.get("LOG_LEVEL")
+LOG_LEVEL = os.environ.get("LOG_LEVEL")
+LOG_FILE = os.environ.get("LOG_FILE")
