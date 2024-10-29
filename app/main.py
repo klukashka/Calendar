61284db9d884aaf7367e50df61c063c3bbb4cc9f
klukashka <klukin0202@yandex.ru>
2024-10-30 00:04:42 +0300

    Redis storage was added, Cache-clearing job was added

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import logging
 import asyncio
+from datetime import datetime
 import uvicorn
+import tzlocal
 from fastapi.middleware.cors import CORSMiddleware
 from app.db.connector import setup_get_pool
 from fastapi import FastAPI
@@ -13,25 +15,33 @@ from app.core.email_sender import EmailSender
 from app.core.routers_includer import include_routers
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.interval import IntervalTrigger
-import tzlocal
+from app.db.redis_storage import RedisStorage
 
 
 async def main() -> None:
     """Main function to run app"""
+
     logging.basicConfig(
         format='%(asctime)s : %(levelname)s : %(message)s',
         level=LOG_LEVEL,
-        filename="test.log",
+        filename=LOG_FILE,
     )
 
     app = FastAPI()
 
     session_pool = await setup_get_pool(DB_URL)
 
+    redis_pool = RedisStorage(
+        host=REDIS_HOST,
+        port=int(REDIS_PORT),
+        db=int(REDIS_DB),
+        password=REDIS_PASS,
+    )
+    await redis_pool.connect()
+
     origins = [
         f"http://{FRONT_HOST}:{FRONT_PORT}",
     ]
-
     app.add_middleware(
         CORSMiddleware,
         allow_origins=origins,
@@ -45,15 +55,22 @@ async def main() -> None:
         [auth_backend],
     )
 
-    await include_routers(app, users, session_pool) # includes all the routers
+    await include_routers(app, users, session_pool, redis_pool)
 
     email_sender = EmailSender(session_pool, ADMIN_EMAIL, ADMIN_EMAIL_PASSWORD, SMTP_PORT, SMTP_SERVER)
     await email_sender.start()
 
     scheduler = _init_scheduler()
+
+    scheduler.add_job(
+        redis_pool.clear_cache,
+        IntervalTrigger(seconds=30),
+        next_run_time=datetime.now(),
+    )
     scheduler.add_job(
         email_sender.distribute_emails,
-        IntervalTrigger(seconds=60),
+        IntervalTrigger(seconds=30),
+        next_run_time=datetime.now(),
     )
 
     try:
@@ -61,7 +78,9 @@ async def main() -> None:
         server = uvicorn.Server(config)
         await server.serve()
     finally:
-       await email_sender.close()
+        await email_sender.close()
+        await redis_pool.close()
+
 
 def _init_scheduler() -> AsyncIOScheduler:
     """
