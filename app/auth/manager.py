77591ec03a237dce535ee59df4482c8ab9e1eb4f
klukashka <klukin0202@yandex.ru>
2025-02-13 23:49:06 +0300

    Removed hardcoded secret_key, added docstrings and type hints

--- app/auth/manager.py
+++ app/auth/manager.py
@@ -1,22 +1,32 @@
-from typing import AsyncGenerator, Any
+from typing import AsyncGenerator, Any, Callable
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 from fastapi import Depends
 from fastapi_users import BaseUserManager, IntegerIDMixin
+from fastapi_users.db import BaseUserDatabase
 from app.models.user import User
 from fastapi_users.db import SQLAlchemyUserDatabase
-from app.config import SECRET_KEY
 
 
 class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
     """User info manager (concerning registration and authentication)"""
-    reset_password_token_secret = SECRET_KEY
-    verification_token_secret = SECRET_KEY
+    reset_password_token_secret: str
+    verification_token_secret: str
+
+    def __init__(self, user_db: BaseUserDatabase[User, int], secret_key: str):
+        super().__init__(user_db)
+        self.reset_password_token_secret = secret_key
+        self.verification_token_secret = secret_key
 
 
-async def providing_user_manager(session_maker: async_sessionmaker[AsyncSession]):
+async def providing_user_manager(
+        session_maker: async_sessionmaker[AsyncSession],
+        secret_key: str) -> (
+        Callable[[BaseUserDatabase[User, int]], AsyncGenerator[UserManager, Any]]
+):
     """
     Function to inject the user manager class instance into FastAPIUsers.
     :param session_maker: async_session_maker to standardize how sessions are configured
+    :param secret_key: credential that is used by authentication endpoints to generate additional credentials
     :return get_user_manager: Returns a function to insert a generator into FastAPIUsers.
     """
 
@@ -24,10 +34,14 @@ async def providing_user_manager(session_maker: async_sessionmaker[AsyncSession]
         async with session_maker() as session:
             yield session
 
-    async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    async def get_user_db(session: AsyncSession = Depends(get_async_session)) -> (
+            SQLAlchemyUserDatabase[AsyncSession, User]
+    ):
         yield SQLAlchemyUserDatabase(session, User)
 
-    async def get_user_manager(user_db=Depends(get_user_db)) -> AsyncGenerator[UserManager, Any]:
-        yield UserManager(user_db)
+    async def get_user_manager(
+            user_db: BaseUserDatabase[User, int] = Depends(get_user_db)) -> (
+            AsyncGenerator)[UserManager, Any]:
+        yield UserManager(user_db, secret_key)
 
     return get_user_manager
